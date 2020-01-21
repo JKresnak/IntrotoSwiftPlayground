aa25db4edb8298f6e30b621cb70e4bca7e5aeb0d
JKresnak <Jkresnak@hawaii.edu>
2020-01-21 10:21:59 -1000

    SwiftPlayground Attempts Jan 21 2020

--- SwiftPlaygroundsOne.playground/Contents.swift
+++ SwiftPlaygroundsOne.playground/Contents.swift
@@ -4,4 +4,11 @@ var str = "Hello, playground"
 
 var myFirstVariable:String
 
-myFirstVariable
+myFirstVariable =  "First string, eskitit jk thats cringe"
+
+//Delcare and assign variable in single line of code
+var mySecondVariable = "2 strings copy teacher"
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+
+print(myThirdVariable)
