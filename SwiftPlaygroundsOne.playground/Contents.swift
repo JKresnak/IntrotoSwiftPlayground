a655b39606bfa08729f0cc83d972c147467e8d8f
JKresnak <Jkresnak@hawaii.edu>
2020-01-21 10:43:54 -1000

    Swift Playground Part 8

--- SwiftPlaygroundsOne.playground/Contents.swift
+++ SwiftPlaygroundsOne.playground/Contents.swift
@@ -12,3 +12,15 @@ var mySecondVariable = "2 strings copy teacher"
 var myThirdVariable = myFirstVariable + mySecondVariable
 
 print(myThirdVariable)
+
+//Part 8
+
+var Lykos = "Another one"
+
+var Nightowl = "aanother one"
+
+var Zao = "And another one"
+
+var Khalid = Lykos + Nightowl + Zao
+
+print(Khalid)
